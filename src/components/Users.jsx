65210ed16a197197b3e7a560239afc8e93fc161b
Sumi Akter <sumiwdcse@gmail.com>
2025-10-30 13:27:50 +0600

    practice module 53 part 8

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -4,21 +4,41 @@ import { use } from 'react';
 const Users = ({usersPromise}) => {
     const users = use(usersPromise);
     console.log(users)
+
+    const handleAddUser = (e) =>{
+        e.preventDefault();
+        const name = e.target.name.value;
+        const email = e.target.email.value;
+        console.log(name,email);
+        const newUser = {name,email};
+        //send data to the  server
+        fetch('http://localhost:3000/users', {
+            method:'POST',
+            headers:{
+               "content-type":'application/json'
+            },
+            body: JSON.stringify(newUser)
+        } )
+        .then(res => res.json())
+        .then(data => {
+            console.log('after post ', data);
+        })
+    }
     return (
     
         <div>
             <div>
                 <h3>Add a user</h3>
-                <form>
-                    <input type="text" />
+                <form onSubmit={handleAddUser}>
+                    <input name='name' type="text" />
                     <br />
-                    <input type="email" name="" id="" />
+                    <input type="email" name="email" id="" />
                     <br />
                     <button>Add user</button>
                 </form>
             </div>
             {
-                users.map(user => <p>{user.name}</p>)
+                users.map(user => <p>{user.name} {user.email}</p>)
             }
         </div>
     );
